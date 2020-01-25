3cd4f5a4cffd36d3e1066274ee12b0749e5096e3
Iury Vieira Lippo da Silva <iuryvieira.lippo@gmail.com>
2020-01-25 01:55:13 -0300

    grid com paginacao

--- src/components/Pedidos/PedidosLista.js
+++ src/components/Pedidos/PedidosLista.js
@@ -1,29 +1,90 @@
 import React, { Component }from 'react';
 import { DataGrid, GridColumn, GridColumnGroup, GridHeaderRow } from 'rc-easyui';
-import dados from './dados';
+import {dados, footer} from './dados';
 import './PedidosLista.css';
 
 class PedidosLista extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
-            data: dados
+          total: 0,
+          pageNumber: 1,
+          pageSize: 10,
+          data: [],
+          loading: false,
+          pagePosition: "bottom",
+          pageOptions: {
+            layout: ['list', 'sep', 'first', 'prev', 'next', 'last', 'sep', 'refresh', 'sep', 'manual', 'info']
+          },
+          footerData: footer,
+        }
+      }
+
+      componentDidMount() {
+        this.loadPage(this.state.pageNumber, this.state.pageSize)
+      }
+
+      loadPage(pageNumber, pageSize) {
+        this.setState({ loading: true })
+        setTimeout(() => {
+          let result = this.getData(pageNumber, pageSize);
+          this.setState(Object.assign({}, result, {
+            data: result.rows,
+            loading: false
+          }))
+        }, 1000);
+      }
+
+      getData(pageNumber, pageSize) {
+        let total = dados.length;
+        let data = [];
+        let start = (pageNumber - 1) * pageSize;
+        
+        for (let i = start + 1; i <= start + pageSize; i++) {
+            if(i > total) {
+                break;
+            }
+            console.log(dados[i-1]);
+            data.push(dados[i-1]);
         }
 
+        return {
+          total: total,
+          pageNumber: pageNumber,
+          pageSize: pageSize,
+          rows: data
+        };
+      }
+
+    handlePageChange(event) {
+    this.loadPage(event.pageNumber, event.pageSize)
     }
-    
-    
 
     render() {
-        console.log(dados);
         return (
             <div className="PedidosLista">
-                <h1>Lista de pedidos</h1>
-                <DataGrid data={this.state.data} columnResizing  className="PedidosLista-DataGrid">
-                    <GridColumnGroup frozen width={120}>
+                <DataGrid 
+                    showFooter
+                    data={this.state.data} 
+                    footerData={this.state.footerData}
+                    className="PedidosLista-DataGrid"
+                    columnResizing  
+                    pagination
+                    lazy
+                    {...this.state}
+                    onPageChange={this.handlePageChange.bind(this)}
+                    >
+                    <GridColumnGroup frozen width={310}>
                         <GridHeaderRow>
-                            <GridColumn field="pedidoNumero" title="Número"></GridColumn>
+                            <GridColumn field="rn" align="center" width="30px"
+                                cellCss="datagrid-td-rownumber"
+                                render={({rowIndex}) => (
+                                <span>{rowIndex+1}</span>
+                                )}
+                            />
+                            <GridColumn field="titulo"title="Gerar PDF" width={80}></GridColumn>
+                            <GridColumn field="total" title="Exportar" width={80}></GridColumn>
+                            <GridColumn field="pedidoNumero" title="Número" width={120}></GridColumn>
                         </GridHeaderRow>
                     </GridColumnGroup>
                     <GridColumnGroup>
